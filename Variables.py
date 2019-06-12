a6222e75befcca57a697ed040bff1f8fa890dc99
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-06-12 08:57:35 +0530

    variables and constants

---
+++ Variables.py
@@ -0,0 +1,28 @@
+import constant
+
+var = 10
+print(var)
+print(type(var))
+
+var = 20.30
+print(var)
+print(type(var))
+
+codekulPython = 13
+
+res = var + codekulPython
+
+print(res)
+print(type(res))
+
+str1 = 'CodeKul'
+
+# res = var + str1
+
+TEN = 10
+
+print(constant.ONE)
+print(constant.TWO)
+
+constant.ONE = 10
+print(constant.ONE)

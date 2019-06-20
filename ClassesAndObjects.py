c5b85f99c38c316dc8043e166487be938619b33d
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-06-20 12:59:47 +0530

    classes and objects

---
+++ ClassesAndObjects.py
@@ -0,0 +1,16 @@
+class MyClass:
+    name = ""
+
+    def display(self):
+        print("Name:",self.name)
+
+obj1 = MyClass()
+obj1.name = "Codekul"
+obj1.display()
+
+
+obj2 = MyClass()
+obj2.name = "Python"
+obj2.display()
+obj1.display()
+

d3a99ce3090b1ab11b627fa63cc1f9e9c27f72bf
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-07-17 11:53:57 +0530

    some more demos

---
+++ DatabaseConnectivity.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+mydb = mysql.connector.connect (
+    host="localhost",
+    user="root",
+    passwd="root@123",
+    database="StudentDB"
+)
+mycursor = mydb.cursor()
+
+
+# # create table
+# mycursor.execute("CREATE TABLE Student (name VARCHAR(255), marks VARCHAR(255))")
+# print("Table is created...")
+
+
+# # Insert into table
+# sql = "INSERT INTO Student (name, marks) VALUES (%s, %s)"
+# val = ("PQR", "90")
+# mycursor.execute(sql, val)
+# mydb.commit()
+# print("data inserted...")
+
+
+# # Select from table
+# mycursor.execute("SELECT * FROM Student")
+# myresult = mycursor.fetchall()
+# for x in myresult:
+#     print(x)
+
+
+# Where clause
+sql = "SELECT * FROM Student WHERE marks ='90'"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+    print(x)
---
+++ NumPyDemo.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.arange(18).reshape(6, 3)
+print(arr)
+
+print(arr.shape)
+
+print(type(arr))

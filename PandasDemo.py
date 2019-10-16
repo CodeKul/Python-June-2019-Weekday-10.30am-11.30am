9580051d34232cc925aaa6fc4d493eb45a7710f8
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-10-16 11:29:23 +0530

    pandas demos

--- PandasDemo.py
+++ PandasDemo.py
@@ -1,10 +1,49 @@
 # 1) Series
 # 2) DataFrame
+# 3) Panel
 
 import pandas as pd
 
-dict1 = {'Python': [20, 40, 25, 63, 54, 34, 34, 67, 89, 23, 45, 78], 'Java': [45, 42, 64, 72, 57, 35, 78, 34, 78, 56, 45, 73], 'C': [34, 54, 23, 67, 23, 34, 56, 53, 36, 89, 45, 34], 'C++': [53, 64, 74, 34, 86, 36, 45, 34, 65, 73, 67, 45], 'Data structures': [45, 45, 56, 56, 34, 45, 56, 23, 78, 78, 67, 23], 'Android': [45, 54, 87, 23, 97, 56, 34, 68, 46, 23, 67, 67], 'iOS': [54, 56, 47, 84, 46, 56, 67, 34, 56, 34, 56, 67]}
+ser1 = pd.Series(['Red', 'Green', 'Blue', 'Yellow', 'Magenta', 'Cyan', 'Black', 'White', 'Gray'])
+
+ser2 = pd.Series([1,2,3,4,5,6,7,8,9])
+# print(ser2.Two)
+
+
+df = pd.DataFrame([ser1, ser2])
+
+dict1 = {'Python': [20, 40, 25, 63, 54, 34, 34, 67, 89, 23, 45, 78], 'Java': [45, 42, 64, 72, 57, 35, 78, 34, 78, 56, 45, 73],'C': [34, 54, 23, 67, 23, 34, 56, 53, 36, 89, 45, 34], 'C++': [53, 64, 74, 34, 86, 36, 45, 34, 65, 73, 67, 45], 'Data structures': [45, 45, 56, 56, None, 45, 56, 23, 78, 78, 67, 23], 'Android': [45, 54, 87, 23, 97, 56, 34, 68, 46, 23, 67, 67], 'iOS': [54, 56, 47, 84, 46, 56, 67, 34, 56, 34, 56, 67]}
 
 df = pd.DataFrame(dict1, index=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'])
+# print(df)
+
+# print(df.head(3))
+# print(df.tail(10))
+
+# print(df.shape)
+
+# df.info(null_counts=True)
+
+# print(df.mean())
+
+# print(df.max())
+# print(df.min())
+
+# print(df.std())
+
+# print(df.median())
+# print(df.count())
+
+# print(df.describe())
+
+df.rename(columns={'Data structures':'Data_structures'}, inplace=True)
+df.Data_structures = df.Data_structures.fillna(int(df.Data_structures.mean()))
+
+df.drop(columns=['C++'], inplace=True)
+
+# df = df[['Python', 'Java', 'C']].corr()
+
+df.Data_structures = df.Data_structures.astype(int) 
+
 print(df)
-print(df.min())
+

bc080165a58ed5dda550ab7d312f8079835b01b5
Varun Kudalkar <varun.kudalkar@gmail.com>
2019-11-07 11:11:08 +0530

    some more numpy demos

--- NumPyDemo.py
+++ NumPyDemo.py
@@ -86,3 +86,63 @@ print('Original Array:\n', arr)
 # cumulative sum along each row 
 print ("Cumulative sum along each row:\n", arr.cumsum(axis = 1))
 
+
+a = np.array([[1, 2], 
+            [3, 4]]) 
+b = np.array([[4, 3], 
+            [2, 1]]) 
+  
+# add arrays 
+print ("Array sum:\n", a + b) 
+
+# multiply arrays (elementwise multiplication) 
+print ("Array multiplication:\n", a*b) 
+
+# matrix multiplication 
+print ("Matrix multiplication:\n", a.dot(b)) 
+
+# create an array of sine values 
+a = np.array([0, np.pi/2, np.pi]) 
+print ("Sine values of array elements:", np.sin(a)) 
+print ("Cos values of array elements:", np.cos(a)) 
+
+# exponential values 
+a = np.array([0, 1, 2, 3]) 
+print ("Exponent of array elements:", np.exp(a)) 
+
+# square root of array values 
+print ("Square root of array elements:", np.sqrt(a)) 
+
+
+# Sorting array:
+
+a = np.array([  [1, 4, 2], 
+                [3, 4, 6], 
+                [0, -1, 5]]) 
+  
+# sorted array 
+print ("Array elements in sorted order:\n", 
+                    np.sort(a, axis = None)) 
+
+# sort array row-wise 
+print ("Row-wise sorted array:\n", 
+                np.sort(a, axis = 0)) 
+
+# specify sort algorithm 
+print ("Column wise sort by applying merge-sort:\n", np.sort(a, axis = 0, kind = 'mergesort')) 
+
+# Example to show sorting of structured array 
+# set alias names for dtypes 
+dtypes = [('name', 'S10'), ('grad_year', int), ('cgpa', float)] 
+
+# Values to be put in array 
+values = [('Hrithik', 2009, 8.5), ('Ajay', 2008, 8.7),  
+           ('Pankaj', 2008, 7.9), ('Aakash', 2009, 9.0)] 
+
+# Creating array 
+arr = np.array(values, dtype = dtypes) 
+print ("\nArray sorted by names:\n", 
+            np.sort(arr, order = 'name')) 
+              
+print ("Array sorted by grauation year and then cgpa:\n", 
+                np.sort(arr, order = ['grad_year', 'cgpa'])) 
